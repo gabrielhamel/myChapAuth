e85825b35a9ed6bbc3886f6277d4108f7778a163
gabrielhamel <gabriel.hamel@epitech.eu>
2019-05-05 13:50:31 +0200

    Better read

--- src/send.c
+++ src/send.c
@@ -33,13 +33,20 @@ static size_t make_ip_hdr(cli_t *cli, struct iphdr *packet, size_t size)
 ssize_t socket_write(cli_t *cli, void *buf, size_t count)
 {
     uint8_t packet[ETH_DATA_LEN] = {0};
+    socklen_t len = sizeof(struct sockaddr_in);
+    in_port_t port = cli->daddr.sin_port;
     size_t size = 0;
+    ssize_t res;
 
     memcpy(packet + sizeof(struct iphdr) + sizeof(struct udphdr), buf, count);
     size = make_udp_hdr(cli, (void *)packet + sizeof(struct iphdr), count);
     size = make_ip_hdr(cli, (void *)packet, size);
-    return (sendto(cli->ext, packet, size, 0, (struct sockaddr *)
-    &(cli->daddr), sizeof(struct sockaddr_in)));
+    res = sendto(cli->ext, packet, size, 0, (struct sockaddr *)
+    &(cli->daddr), sizeof(struct sockaddr_in));
+    recvfrom(cli->ext, packet, res, 0,
+    (struct sockaddr *)&(cli->daddr), &len);
+    cli->daddr.sin_port = port;
+    return (res);
 }
 
 ssize_t socket_read(cli_t *cli, void *buf, size_t count)
@@ -49,14 +56,10 @@ ssize_t socket_read(cli_t *cli, void *buf, size_t count)
     ssize_t res;
     in_port_t port = cli->daddr.sin_port;
 
-    recvfrom(cli->ext, packet, ETH_DATA_LEN, 0,
-    (struct sockaddr *)&(cli->daddr), &size);
-    cli->daddr.sin_port = port;
     res = recvfrom(cli->ext, packet, ETH_DATA_LEN, 0,
     (struct sockaddr *)&(cli->daddr), &size);
-    if (res > 0)
-        memcpy(buf, packet + sizeof(struct iphdr) +
-        sizeof(struct udphdr), count);
+    memcpy(buf, packet + sizeof(struct iphdr) +
+    sizeof(struct udphdr), count);
     cli->daddr.sin_port = port;
     return (res - sizeof(struct iphdr) - sizeof(struct udphdr));
 }
